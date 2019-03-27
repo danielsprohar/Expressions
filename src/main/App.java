3326f113111390ce8bbb06df4e2471bab17fcf10
MrNullPtr <danielsprohar@outlook.com>
2019-03-27 18:47:13 -0500

    changes

--- src/main/App.java
+++ src/main/App.java
@@ -9,28 +9,25 @@ import expression.MalformedExpressionException;
 public class App {
 
 	public static void main(String[] args) throws MalformedExpressionException {
-		
-		InfixExpression infixExpression = InfixExpression.create("x - y - z * (a + b)");
-		System.out.println(infixExpression.toPrefix());
 
-//		List<InfixExpression> infixExpressions = new ArrayList<>();
-//		// page 339
-//		infixExpressions.add(InfixExpression.create("a - b + c * d")); // ab-cd*+
-//		infixExpressions.add(InfixExpression.create("a + c - h/b * r")); // ac+hb/r*-
-//		infixExpressions.add(InfixExpression.create("a + (c - h) / (b * r)")); // ach-br*/+
-//		infixExpressions.add(InfixExpression.create("(a + b) * c - (d + e * f/((g/h + i - j) * k))/ r"));
-//		
-//		String dashedLine = printDashedLine(110);
-//		System.out.println(dashedLine);
-//		System.out.format("%-75s%-50s%n", "Infix", "Postfix");
-//		System.out.println(dashedLine);
-//		
-//		for (InfixExpression expression : infixExpressions) {
-//			System.out.format("%-75s%-50s%n", expression, expression.toPostfix());
-//			System.out.println(dashedLine);
-//		}
+		List<InfixExpression> infixExpressions = new ArrayList<>();
+		// page 339
+		infixExpressions.add(InfixExpression.create("x + y * z"));
+		infixExpressions.add(InfixExpression.create("(x + y) * z"));
+		infixExpressions.add(InfixExpression.create("x - y - z * (a + b)"));
+		infixExpressions.add(InfixExpression.create("(a + b) * c - (d + e * f/((g/h + i - j) * k))/ r"));
+
+		String dashedLine = printDashedLine(150);
+		System.out.println(dashedLine);
+		System.out.format("%-75s%-50s%-50s%n", "Infix", "Postfix", "Prefix");
+		System.out.println(dashedLine);
+
+		for (InfixExpression expression : infixExpressions) {
+			System.out.format("%-75s%-50s%-50s%n", expression, expression.toPostfix(), expression.toPrefix());
+			System.out.println(dashedLine);
+		}
 	}
-	
+
 	private static String printDashedLine(int count) {
 		StringBuilder sb = new StringBuilder(count);
 		for (int i = 0; i < count; i++) {
